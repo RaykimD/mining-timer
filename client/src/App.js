ae5f95d32dda5a36d732a97458447c42ce6429b9
RaykimD <raykimdh@naver.com>
2024-11-12 10:34:35 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import logo from './images/logo.png';
 
 function App() {
